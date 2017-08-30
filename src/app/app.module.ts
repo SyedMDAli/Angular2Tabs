fb0c612a894c130560fb88f2e75f7fbf1d6039ca
Syed ali <syed.ma@ziniostech.com>
2017-08-30 11:33:10 +0530

    tabs initial set up

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+import {Tabs} from './tabs';
+import {Tab} from './tab';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Tabs, 
+    Tab
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
